073e0d276ff832ffdce3d7d371728111d34f03ff
AminLakhal <64310291+AminLakhal@users.noreply.github.com>
2022-10-11 20:46:49 +0200

    add: L01_Zufallsgedicht

---
+++ Aufgaben/L01_Zufallsgedicht/script.js
@@ -0,0 +1,45 @@
+//# sourceMappingURL=script.js.map
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+var bausteine = {
+    Subjekt: [
+        "Ein Täter",
+        "Jemand",
+        "Der Einbrecher",
+        "Er",
+        "Polizei",
+    ],
+    Prädikat: [
+        "bricht",
+        "erwischt",
+        "verschwindet",
+        "vernichtet",
+        "richtet",
+    ],
+    Objekt: [
+        "ins Haus ein",
+        "den Täter",
+        "vom Tatort",
+        "Spuren",
+        "Täter",
+    ],
+};
+let x = getRandomInt(bausteine.Subjekt.length);
+let y = getRandomInt(bausteine.Prädikat.length);
+let z = getRandomInt(bausteine.Objekt.length);
+for (let i = 0; i < 5; i++) {
+    console.log(bausteine.Subjekt[x] + " " + bausteine.Prädikat[y] + " " + bausteine.Objekt[z]);
+    const index = bausteine.Subjekt.indexOf(bausteine.Subjekt[x]);
+    const index2 = bausteine.Objekt.indexOf(bausteine.Objekt[y]);
+    const index3 = bausteine.Prädikat.indexOf(bausteine.Prädikat[z]);
+    if (index !== -1) {
+        bausteine.Subjekt.splice(index, 1);
+        bausteine.Objekt.splice(index, 1);
+        bausteine.Prädikat.splice(index, 1);
+    }
+    x = getRandomInt(bausteine.Subjekt.length);
+    y = getRandomInt(bausteine.Prädikat.length);
+    z = getRandomInt(bausteine.Objekt.length);
+}
+//# sourceMappingURL=script.js.map
